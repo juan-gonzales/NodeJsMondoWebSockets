9ef0536e18fdffa6ddf76cc6ba44128e74e68616
Juan gonzales <juanraul.gonzales@gmail.com>
2022-09-05 16:09:01 -0500

    segunda clase

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express=require('express');
+const router=express.Router();
+
 
 var app=express();
 
-app.use('/',function(req,res){
+// app.use('/',function(req,res){
+//     res.send('Hello World');
+// });
+
+app.use(router);
+router.get('/',function(req,res){
     res.send('Hello World');
+});
+
+router.post('/',function(req,res){
+    res.send('Hello World');
+});
+
+router.get('/message',function(req,res){
+    res.send('Lista de mensaje');
+});
+
+router.post('/message',function(req,res){
+    res.send('Mensaje añadido');
 });
 
 app.listen(3000,function(){
