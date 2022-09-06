eaf9a79b0ecd860051bc15d1e4a0ee5ea9416a90
Juan gonzales <juanraul.gonzales@gmail.com>
2022-09-05 21:42:00 -0500

    octava clase

--- components/message/store.js
+++ components/message/store.js
@@ -1,14 +1,25 @@
-const list= [];
+const db=require('mongoose');
+const Model=require('./model');
+
+db.promise = global.promise;
+db.connect('mongodb+srv://admin:jR7zD1znOPjRDDyy@cluster0.vsaka.mongodb.net/node?retryWrites=true&w=majority',{
+    useNewUrlParser:true,
+});
+console.log('db conectada');
 
 function addMessage(message) {
-    list.push(message);
+    const myMessage=new Model(message);
+    myMessage.save();
 };
 
-function getMessages() {
-    return list;
+async function getMessages() {
+    const messages=await Model.find();
+    return messages;
 };
 
 module.exports = {
     add: addMessage, 
     list: getMessages
 };
+
+//jR7zD1znOPjRDDyy
